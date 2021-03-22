bcf9c50b9ac44561588094dfb4e495a2f16a1e64
IvanBakutin <80706145+IvanBakutin@users.noreply.github.com>
2021-03-22 18:39:12 +0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { LoadAppComponent } from './load-app/load-app.component';
+
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    declarations: [   AppComponent, LoadAppComponent ],
+    bootstrap:    [  AppComponent ]
 })
 export class AppModule { }
